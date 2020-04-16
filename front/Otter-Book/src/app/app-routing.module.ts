e8c7bcc00ba3f511bac25940e344b8cd9b85b359
OtterWorld <yegor.gagarinov@gmail.com>
2020-04-16 06:27:24 +0600

    router first layout

--- front/Otter-Book/src/app/app-routing.module.ts
+++ front/Otter-Book/src/app/app-routing.module.ts
@@ -2,7 +2,40 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/',
+  },
+  {
+    path:'Websites',
+    children: [
+       {
+         path: ':id'
+       }
+    ]
+  },
+  {
+    path:'FlatDraw'
+  },
+  {
+    path:'PathFinder'
+  },
+  {
+    path:'SortAlgoritms'
+  },
+  {
+    path: "WebsiteScrapper"
+  },
+  {
+    path: 'ThreeJS'
+  },
+  {
+    path: 'WebGL'
+  },
+  {
+    path: 'C++'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
